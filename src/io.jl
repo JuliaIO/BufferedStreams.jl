18c2b7aa230aca8e1ec357cffaeacc17298a8633
Kenta Sato <bicycle1885@gmail.com>
2016-06-19 01:35:58 +0900

    do not use pointer_to_array tricks

--- src/io.jl
+++ src/io.jl
@@ -2,14 +2,11 @@
 # --
 
 function readbytes!(source::IO, buffer::AbstractArray{UInt8}, from::Int, to::Int)
-    return Base.readbytes!(
-        source,
-        pointer_to_array(pointer(buffer, from), (to - from + 1,)),
-        to - from + 1)
+    return Base.readbytes!(source, sub(buffer, from:to), to - from + 1)
 end
 
 function writebytes(sink::IO, buffer::AbstractArray{UInt8}, n::Int, eof::Bool)
-    return write(sink, pointer_to_array(pointer(buffer), (n,)))
+    return write(sink, sub(buffer, 1:n))
 end
 
 
