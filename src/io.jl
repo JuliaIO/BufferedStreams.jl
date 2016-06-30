78ad4ce413919da51106c66680fb169de89cb39a
Kenta Sato <bicycle1885@gmail.com>
2016-07-01 07:23:26 +0900

    replace sub with view

--- src/io.jl
+++ src/io.jl
@@ -2,11 +2,11 @@
 # --
 
 function readbytes!(source::IO, buffer::AbstractArray{UInt8}, from::Int, to::Int)
-    return Base.readbytes!(source, sub(buffer, from:to), to - from + 1)
+    return Base.readbytes!(source, view(buffer, from:to), to - from + 1)
 end
 
 function writebytes(sink::IO, buffer::AbstractArray{UInt8}, n::Int, eof::Bool)
-    return write(sink, sub(buffer, 1:n))
+    return write(sink, view(buffer, 1:n))
 end
 
 
