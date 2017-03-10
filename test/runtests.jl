47762a1e91e3776f26830f8149fb02c7f233a917
Kenta Sato <bicycle1885@gmail.com>
2017-03-10 17:11:22 +0900

    julia 0.4

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,9 +21,11 @@ function Base.read(stream::InfiniteStream, ::Type{UInt8})
     return stream.byte
 end
 
-function Base.unsafe_read(stream::InfiniteStream, pointer::Ptr{UInt8}, n::UInt)
-    ccall(:memset, Void, (Ptr{Void}, Cint, Csize_t), pointer, stream.byte, n)
-    return nothing
+if VERSION >= v"0.5-"
+    function Base.unsafe_read(stream::InfiniteStream, pointer::Ptr{UInt8}, n::UInt)
+        ccall(:memset, Void, (Ptr{Void}, Cint, Csize_t), pointer, stream.byte, n)
+        return nothing
+    end
 end
 
 
